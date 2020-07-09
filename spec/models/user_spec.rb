16eed373afe9837fdd6c1787269c331cdcf2fa3b
Sourav Goswami <souravgoswami@protonmail.com>
2020-07-10 03:30:39 +0530

    Implemented custom full_name method to user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,5 +20,11 @@ RSpec.describe User, type: :model do
 			@user.first_name = @user.last_name = nil
 			expect(@user).to_not be_valid
 		end
+
+		describe 'custom name method' do
+			it 'has full name method that combines first name and last name' do
+				expect(@user.full_name).to eq('SNOW, JOHN')
+			end
+		end
 	end
 end
