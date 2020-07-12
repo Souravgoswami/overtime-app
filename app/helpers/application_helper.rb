6c4bf4eeb14be361c5cdd9993d237e1370c0f845
Sourav Goswami <souravgoswami@protonmail.com>
2020-07-12 06:14:08 +0530

    Integrated custom notifications for ActiveRecord validations and flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
 	def active?(path)
 		"active" if current_page?(path)
 	end
+
+	def flash_message_manager
+		msg = flash[:notice] || flash[:alert] || flash[:error]
+		notify(msg).html_safe if msg
+	end
+
+	def notify(msg, btn_msg = 'Ok')
+		%Q[<script>notify("#{msg}", "#{btn_msg}" )</script>].html_safe
+	end
 end
