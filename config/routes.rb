bad72946b837a39ede9eda173fbf121710945eff
Sourav Goswami <souravgoswami@protonmail.com>
2020-07-13 04:31:04 +0530

    Removed registration functionality from app

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 	resources :posts
 	get 'posts/:id/edit', to: 'posts#edit', as: :edit_post_path
-	devise_for :users
+	devise_for :users, skip: [:registrations]
 	root to: 'static#homepage'
 	resources :static
 end
