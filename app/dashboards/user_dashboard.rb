7fd55c749c3b8794c08db2af172a588e765dd4ab
Sourav Goswami <souravgoswami@protonmail.com>
2020-07-16 00:44:20 +0530

    Customized search in admin dashboard

--- app/dashboards/user_dashboard.rb
+++ app/dashboards/user_dashboard.rb
@@ -9,17 +9,17 @@ class UserDashboard < Administrate::BaseDashboard
 	# on pages throughout the dashboard.
 	ATTRIBUTE_TYPES = {
 		posts: Field::HasMany,
-		id: Field::Number,
+		id: Field::Number.with_options(searchable: false),
 		email: Field::String,
-		encrypted_password: Field::String,
-		reset_password_token: Field::String,
-		reset_password_sent_at: Field::DateTime,
-		remember_created_at: Field::DateTime,
-		first_name: Field::String,
-		last_name: Field::String,
-		type: Field::String,
-		created_at: Field::DateTime,
-		updated_at: Field::DateTime,
+		encrypted_password: Field::String.with_options(searchable: false),
+		reset_password_token: Field::String.with_options(searchable: false),
+		reset_password_sent_at: Field::DateTime.with_options(searchable: false),
+		remember_created_at: Field::DateTime.with_options(searchable: false),
+		first_name: Field::String.with_options(searchable: false),
+		last_name: Field::String.with_options(searchable: false),
+		type: Field::String.with_options(searchable: false),
+		created_at: Field::DateTime.with_options(searchable: false),
+		updated_at: Field::DateTime.with_options(searchable: false),
 	}.freeze
 
 	# COLLECTION_ATTRIBUTES
@@ -28,27 +28,26 @@ class UserDashboard < Administrate::BaseDashboard
 	# By default, it's limited to four items to reduce clutter on index pages.
 	# Feel free to add, remove, or rearrange items.
 	COLLECTION_ATTRIBUTES = %i[
-		id
-		email
 		posts
+		email
 		type
 	].freeze
 
 	# SHOW_PAGE_ATTRIBUTES
 	# an array of attributes that will be displayed on the model's show page.
 	SHOW_PAGE_ATTRIBUTES = %i[
-	posts
-	id
-	email
-	encrypted_password
-	reset_password_token
-	reset_password_sent_at
-	remember_created_at
-	first_name
-	last_name
-	type
-	created_at
-	updated_at
+		posts
+		id
+		email
+		encrypted_password
+		reset_password_token
+		reset_password_sent_at
+		remember_created_at
+		first_name
+		last_name
+		type
+		created_at
+		updated_at
 	].freeze
 
 	# FORM_ATTRIBUTES
