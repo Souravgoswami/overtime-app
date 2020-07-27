c07f200f39d4a4ec2cd5ab4c96bb9b7beab7f884
Sourav Goswami <souravgoswami@protonmail.com>
2020-07-28 04:17:30 +0530

    Implemented callback to update log when post is created!

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,17 @@ class Post < ApplicationRecord
 	enum status: { submitted: 0, approved: 1, rejected: 2 }
 
 	scope :posts_by, ->user { where(user_id: user.id) }
+
+	after_save :update_audit_log
+
+	private
+
+	def update_audit_log
+		audit_log = AuditLog.where(
+			user_id: self.user_id,
+			start_date: (self.date - 7.days .. self.date)
+		)
+
+		audit_log.map(&:confirmed!)
+	end
 end
