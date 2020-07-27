417f19f94fe45fe0ab99e6dd06154451146e4a66
Sourav Goswami <souravgoswami@protonmail.com>
2020-07-28 04:38:01 +0530

    Integrated basic manager notification email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'souravgoswami24111997s@protonmail.com'
   layout 'mailer'
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,10 +31,6 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -59,5 +55,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_caching = true
   config.action_mailer.default_url_options = { host: '0.0.0.0', port: 8080 }
 end
