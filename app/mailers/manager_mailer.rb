d66b916f13b3cb446e45f0814a692cb2bb39fbb9
Sourav Goswami <souravgoswami@protonmail.com>
2020-07-28 09:41:42 +0530

    Built rake tasks for sending emails

--- app/mailers/manager_mailer.rb
+++ app/mailers/manager_mailer.rb
@@ -1,6 +1,6 @@
 class ManagerMailer < ApplicationMailer
 	def email(manager)
 		@manager = manager
-		mail(to: manager.email, subject: 'Daily overtime request email')
+		mail(to: @manager.email, subject: 'Daily overtime request email')
 	end
 end
